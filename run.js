269cdec821eae0fd1c403d6308500b7672ac908b
shenwl <shenweilun96@gmail.com>
2019-06-19 23:38:26 +0800

    使用错误处理中间件

--- run.js
+++ run.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const InitManager = require('./core/init');
+const catchError = require('./middlewares/exception');
 
 const app = new Koa();
 
 app.use(bodyParser());
+app.use(catchError);
 
 InitManager.initCore(app);
 
