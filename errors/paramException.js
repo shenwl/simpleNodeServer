a2a0e48518fa1d2aef8fbdef029a0bdae72c5e90
shenwl <shenweilun96@gmail.com>
2019-06-19 23:44:21 +0800

    error -rename->exception

--- errors/paramException.js
+++ errors/paramException.js
@@ -3,8 +3,8 @@ const HttpException = require('./httpException');
 const ERROR_CODE = 40000;
 
 class HttpRequestParamError extends HttpException {
-  constructor(paramName, desc, msg) {
-    super(200, desc, ERROR_CODE, `${paramName} wrong: ${msg}`);
+  constructor(paramName, desc = '参数错误', msg = '参数不合法') {
+    super(200, desc, ERROR_CODE, `${paramName}参数错误: ${msg}`);
   }
 }
 
