2259806f024d145ce121ac0b21724cc6325a9898
shenwl <shenweilun96@gmail.com>
2019-06-25 21:53:32 +0800

    msg

--- exceptions/notFoundException.js
+++ exceptions/notFoundException.js
@@ -3,8 +3,8 @@ const HttpException = require('./httpException');
 const ERROR_CODE = 40400;
 
 class ResourceNotFoundError extends HttpException {
-  constructor(resourceName, resourceId) {
-    super(404, ERROR_CODE, `${resourceName}资源未找到, 资源id: ${resourceId}`);
+  constructor(msg = '资源未找到') {
+    super(404, ERROR_CODE, msg);
   }
 }
 
