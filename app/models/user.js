2b73ddfdcea4827dd521b38d14b107193fe6dc25
shenwl <shenweilun96@gmail.com>
2019-06-26 21:10:47 +0800

    verifyEmailPassword成功后返回user

--- app/models/user.js
+++ app/models/user.js
@@ -3,13 +3,27 @@ const bcrypt = require('bcryptjs');
 const { Sequelize, Model } = require('sequelize');
 const { sequelize } = require('../../core/db');
 
+const { AuthFailed } = require('../../exceptions');
+
 const hashPassword = (password) => {
   const salt = bcrypt.genSaltSync(10);
   return bcrypt.hashSync(password, salt);
 };
 
 class User extends Model {
+  static async verifyEmailPassword(email, plainPassword) {
+    const user = await User.findOne({
+      where: {
+        email,
+      }
+    });
+    if (!user) throw new AuthFailed('用户不存在');
 
+    if (!bcrypt.compareSync(plainPassword, user.password)) {
+      if (!user) throw new AuthFailed('密码错误');
+    }
+    return user;
+  }
 }
 
 User.init({
