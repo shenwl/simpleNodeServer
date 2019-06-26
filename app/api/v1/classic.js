773e51a32f9c696622b9a505323e8021a4208606
shenwl <shenweilun96@gmail.com>
2019-06-26 21:35:28 +0800

    使用token验证

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router');
+
+const auth = require('../../../middlewares/auth');
 const router = new Router({
   prefix: '/api/v1/classic'
 });
 
-router.get('/latest', async (ctx) => {
+router.get('/latest', auth, async (ctx) => {
   ctx.body = {
     "content": "人生不能像做菜，把所有的料准备好才下锅",
     "fav_nums": 0,
