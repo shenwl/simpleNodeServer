1f5bf956cb18d631cf2b14a9523ed07da7d19973
shenwl <shenweilun96@gmail.com>
2019-06-27 20:43:12 +0800

    隐私信息放入config.secret.js

--- config/config.js
+++ config/config.js
@@ -1,19 +1,7 @@
-const wx = require('./wx');
+const configSecret = require('./config.secret');
 
 module.exports = {
   // prod or dev
   environment: 'dev',
-  database: {
-    dialect: 'mysql',
-    dbName: 'island',
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '960208',
-  },
-  security: {
-    secretKey: 'i8vD8orMeiWwkvwLgKynTD',
-    expiresIn: 60 * 60 * 2,
-  },
-  wx,
+  ...configSecret,
 };
