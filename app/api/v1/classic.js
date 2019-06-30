86db4b3e31fa679de3bed8723f612c00dac4863b
shenwl <shenweilun96@gmail.com>
2019-06-30 23:39:04 +0800

    latest api

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,17 +1,13 @@
 const Router = require('koa-router');
 
-const auth = require('../../../middlewares/auth');
-const AUTH = require('../../constants/auth');
+const ClassService = require('../../services/classic');
 const router = new Router({
   prefix: '/api/v1/classic'
 });
 
-router.get('/latest', auth(AUTH.ADMIN), async (ctx) => {
-  ctx.body = {
-    "content": "人生不能像做菜，把所有的料准备好才下锅",
-    "fav_nums": 0,
-    "id": 1,
-  };
+router.get('/latest', async (ctx) => {
+  const art = await ClassService.getLatest();
+  ctx.body = art;
 });
 
 module.exports = router;
